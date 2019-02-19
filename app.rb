00bc54884809f370f7d6dcbf4ece86207420a03b
Brent Ransom <sadumedusa@gmail.com>
2019-02-19 12:33:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,17 +25,14 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
   
-  get '/:operation/:number1/:number2' do 
-    
-    ans = "Sorry! This can not be answered."
-    
-    @num1 = params[:number1]
-    @num2 = params[:number2]
-    
-    
-    
+  get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+
+    ans = 'No answer!'
+
     case params[:operation]
-    
+
     when 'add'
       ans = (number1 + number2).to_s
     when 'subtract'
@@ -44,8 +41,7 @@ class App < Sinatra::Base
       ans = (number1 * number2).to_s
     when 'divide'
       ans = (number1 / number2).to_s
-     
+    end
   end
-end
 
 end
